f1daf71b8beebaeeb3e582eadd5ec034f308b91d
Rimu Eirnarn <Rimuru720@proton.me>
2026-07-29 21:48:16 +0700

    a

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ NEXTDNS_API_KEY = "API"
 NEXTDNS_PROFILE_ID = "abc000"
 NEXTDNS_REWRITE_HOSTNAME = "luna.net"
 
-STATE_DIR = os.path.expanduser("$HOME/.luna-service")
+STATE_DIR = os.path.expanduser("~/.luna-service")
 LOG_FILE = os.path.join(STATE_DIR, "luna-service.log")
 LAST_IP_FILE = os.path.join(STATE_DIR, "last_ip")
 LOCK_FILE = os.path.join(STATE_DIR, "run.lock")
